4b22f9c01bf154c425b02ddc3cb592d3a15ec221
lkmandef <lukmanoman17@gmail.com>
2025-06-12 11:50:22 +0800

    membuat objek

---
+++ src/latihan3/Rumus.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan3;
+
+
+public class Rumus {
+    /*
+    *nama :Lukmanul Hakim
+    *npm : 2310010398
+    *kelas :4C
+    */
+    int sisi;
+    int panjang;
+    int lebar;
+}
